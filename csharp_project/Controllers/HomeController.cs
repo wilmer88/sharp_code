216d8e220225267082531ce2b36d41883078d335
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-22 13:18:50 -0400

    added code to view models

--- csharp_project/Controllers/HomeController.cs
+++ csharp_project/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-// using csharp_project.Models;
+using csharp_project.Models;
+// using csharp_project
 
 namespace csharp_project.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly snipR _snipR;
 
-
-    public HomeController()
+    public HomeController(snipR snipReposi)
     {
     //    ProviderAliasAttribute readonly 
+    _snipR = snipReposi;
     }
 
     public IActionResult Index()
     {
+        var snipT = _snipR.GetSnip();
+        // var viewModel = new snipViewModel;
         return View();
     }
 
---
+++ csharp_project/Models/viewmodels/snipViewModel.cs
@@ -0,0 +1,8 @@
+using System;
+namespace csharp_project.Models.snipViewModel
+{
+    public class snipViewModel
+    {
+        public List<Sniipss>? sniipsses {get; set;}
+    }
+}
